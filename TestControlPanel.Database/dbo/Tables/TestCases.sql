1a513b307741fceddecf1406506eb7f6b0a54431
Deepak Aggarwal <deaggarwal@q3tech.com>
2022-12-06 15:19:42 +0530

    [DA]:Added tables

---
+++ TestControlPanel.Database/dbo/Tables/TestCases.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[TestCases] (
+    [Id]              INT            IDENTITY (1, 1) NOT NULL,
+    [TestTitle]       NVARCHAR (MAX) NULL,
+    [TestDate]        DATETIME2 (7)  NOT NULL,
+    [TestDescription] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_TestCases] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+

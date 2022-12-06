20e4462083daed369cefe99488a7a02176e606c6
Deepak Aggarwal <deaggarwal@q3tech.com>
2022-12-06 16:27:24 +0530

    [S]: automapper bug fixed

--- TestControlPanel.Application/Mappings/WeatherProfile.cs
+++ TestControlPanel.Application/Mappings/WeatherProfile.cs
@@ -16,10 +16,10 @@ namespace TestControlPanel.Application.Mappings
         public WeatherProfile()
         {
             CreateMap<WeatherForecastViewModel, WeatherForecast>();
-            CreateMap<IEnumerable<WeatherForecastViewModel>, IEnumerable<WeatherForecast>>();
+            //CreateMap<IEnumerable<WeatherForecastViewModel>, IEnumerable<WeatherForecast>>();
 
             CreateMap<WeatherForecast, WeatherForecastViewModel>();
-            CreateMap<IEnumerable<WeatherForecast>, IEnumerable<WeatherForecastViewModel>>();
+            //CreateMap<IEnumerable<WeatherForecast>, IEnumerable<WeatherForecastViewModel>>();
         }
     }
 }
